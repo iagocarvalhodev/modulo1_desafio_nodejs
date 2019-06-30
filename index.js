8d8167d0064d7d6447a125a9eecca197c557d367
Iago Carvalho <iagocarvalhoweb@icloud.com>
2019-06-30 08:51:27 -0300

    configurando o nunjucks

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const nunjucks = require("nunjucks");
 
 const app = express();
 
+// configurando o nunjucks
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "njk");
+
 app.listen(3001, () => {
   console.log(`Server started on port`);
 });
